c458e3673e9d95082e93ac7a0e2cab45a7f3167a
Denys Vynohradnyi <denv0808@gmail.com>
2023-01-02 18:36:30 +0100

    Added style for navigation, a little bit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,23 +2,31 @@ import { useSelector, useDispatch } from 'react-redux'
 import { loginAction } from '../../redux/features/authentication/authenticationsAction'
 import { MiniProfile } from './MiniProfile';
 import { Navigation } from './Navigation';
+import {
+  logoutAction
+} from '../../redux/features/authentication/authenticationsAction'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   const isLogin = useSelector(state => state.auth.isLogin);
   const dispatch = useDispatch();
- 
+
   return (
     <>
-    <header style={style.header}>
-      {/* logo */}
-      <div style={style.logo}>
-        <img src="https://s2.abcstatics.com/Media/201201/12/OBJ4029626_1--478x440.jpg" alt="logo" width="50px" height="50px"/>
-      </div>
-      <div style={style.iam_and_menu}>
-        {isLogin ? <MiniProfile />: <Login login={()=>dispatch(loginAction())}/>}
-        {isLogin && <Navigation />}   
-      </div>
-    </header>
+      <header style={style.header}>
+        <div style={{ alignSelf:'flex-end'}}>
+          {/* clear ! */}
+          {isLogin && <Navigation />}
+        </div>
+        <div style={style.iam_and_logout}>
+          {isLogin ? <MiniProfile /> : <Login login={() => dispatch(loginAction())} />}
+          {isLogin && <>
+            <NavLink to="/" key={3} onClick={() => dispatch(logoutAction())}>
+              <span>Logout</span>
+            </NavLink>
+          </>}
+        </div>
+      </header>
     </>
   );
 }
@@ -26,36 +34,34 @@ function Header() {
 export default Header;
 
 const style = {
-  header:{
-    backgroundColor: 'grey', 
-    width:"100%", 
-    height:"100px", 
-    display:'flex', 
-    alignItems:'center', 
+  header: {
+    backgroundColor: 'grey',
+    width: "100%",
+    height: "100px",
+    display: 'flex',
+    alignItems: 'center',
     justifyContent: "space-between",
-    borderBottom:'1px solid black'
+    borderBottom: '1px solid black'
 
   },
-  logo:{
-    marginLeft:'15px'
+  signin: {
+    width: "150px",
+    height: "35px",
+    cursor: "pointer",
+    marginRight: "15px"
   },
-  signin:{
-    width:"150px",
-    height:"35px",
-    cursor:"pointer",
-    marginRight:"15px"
-  },
-  iam_and_menu:{
+  iam_and_logout: {
+    // marginLeft: '75%'
     // display:'flex'
   }
 }
 
 const SIGNIN_PHOTO_URL = "https://4.bp.blogspot.com/-tjAWzkiPuLc/VkTU_D9IwbI/AAAAAAAACUg/_dNxgUbTV_Q/s400/signin.png"
 
-const Login = ({login}) => {
+const Login = ({ login }) => {
   return (<>
-  <div>
-    <img onClick={login} style={style.signin} alt="sign in via google" src={SIGNIN_PHOTO_URL}/>
-  </div>
+    <div>
+      <img onClick={login} style={style.signin} alt="sign in via google" src={SIGNIN_PHOTO_URL} />
+    </div>
   </>)
 }
--- client/src/components/Header/Navigation.js
+++ client/src/components/Header/Navigation.js
@@ -1,28 +1,47 @@
 import { NavLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
-import {
-  logoutAction
-} from '../../redux/features/authentication/authenticationsAction'
 
 export const Navigation = (props) => {
 
-  const dispatch = useDispatch();
-
-
-  return (<nav>
-    <NavLink to="/home" key={0} >
-        <span>Welcome</span> 
+  return (
+    <nav style={{ display: 'flex', alignItems:'center'}}>
+      <NavLink to="/file-loader" style={{...style.item, justifyContent: "center"}}>
+        <img src="https://i.ibb.co/42GDgLt/Paper-Waste.png" alt="logo" width="50px" height="50px" />
       </NavLink>
-      <NavLink to="/mailer" key={1} >
-        <span>Mailer</span> 
-      </NavLink>
-      <NavLink to="/file-loader" key={2} >
-        <span>Files</span> 
-      </NavLink> 
-      <NavLink to="/" key={3} onClick={()=>dispatch(logoutAction())}>
-        <span>Logout</span>
-      </NavLink>
-  </nav>)
+      {[{ to: "/home", title: "Welcome" }, { to: "/mailer", title: "Mailer" }, { to: "", title: "Files" }].map((elem, index) => (
+        <NavigationItem {...elem} index={index} />
+      ))}
+    </nav>
+  )
 }
 
 // path={["/home","/offers", "/counterparties", "/staff"]}
+
+const NavigationItem = ({ to, title, index }) => {
+  return (<>
+    <NavLink to={to} key={index} style={style.item}>
+      <span>{title}</span>
+    </NavLink>
+  </>)
+}
+
+const style = {
+  header: {
+    backgroundColor: 'grey',
+    width: "100%",
+    height: "100px",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: "flex-start",
+    borderBottom: '1px solid black'
+  },
+  item: {
+    display: 'flex',   
+    alignItems:'center',
+    justifyContent: "flex-start",
+    width: '150px',
+    height:'70px',
+    backgroundColor: 'lightgray',
+    borderTopRightRadius:'35px',
+    borderTopLeftRadius:'10px',
+  }
+}
